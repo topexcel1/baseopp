c18aa2db6784f939c3391f9aab86dff8808864a1
User <user@MacBook-Pro-61.local>
2016-05-26 16:50:08 -0500

    first commit

---
+++ baseopp/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  baseopp
+//
+//  Created by User on 5/26/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+}
